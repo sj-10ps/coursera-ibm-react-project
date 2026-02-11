67303a00bb6fc25973fc5588b936ec82a856dc48
sj-10ps <sps24723@gmail.com>
2026-02-11 19:47:48 +0530

    fix

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col gap-3 md:max-w-[50%]'>
+           <p className='text-center text-slate-400  md:text-lg capitalize'>welcome to paradise nursery,where green meets serenity</p>
+           <p className=' text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nobis, exercitationem, suscipit dolorum sunt illum voluptas quaerat aliquam molestias quibusdam quasi. A fuga et maxime aliquid temporibus similique suscipit eum.</p>
+                      <p className=' text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nobis, exercitationem, suscipit dolorum sunt illum voluptas quaerat aliquam molestias quibusdam quasi. A fuga et maxime aliquid temporibus similique suscipit eum.</p>
+           <p className=' text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nobis, exercitationem, suscipit dolorum sunt illum voluptas quaerat aliquam molestias quibusdam quasi. A fuga et maxime aliquid temporibus similique suscipit eum.</p>
+
+        </div>
+  )
+}
+
+export default About
